428ec5bd32017d8a42c3224195e13c9625b91435
zjc1985 <yufu@userdeimac.home>
2015-07-28 21:46:07 +0800

    添加注册功能

--- ios_version/MyMapBox/RegisterVC.m
+++ ios_version/MyMapBox/RegisterVC.m
@@ -7,8 +7,16 @@
 //
 
 #import "RegisterVC.h"
+#import "CommonUtil.h"
+#import <AVOSCloud/AVOSCloud.h>
 
 @interface RegisterVC ()
+
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UITextField *repeatTextField;
+@property (weak, nonatomic) IBOutlet UITextField *userNameTextField;
+
 
 @end
 
@@ -23,5 +31,57 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+- (IBAction)registerClick:(id)sender {
+    if ([self isInputValid]) {
+        AVUser *user=[AVUser user];
+        user.email=self.emailTextField.text;
+        user.password=self.passwordTextField.text;
+        user.username=self.userNameTextField.text;
+        [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+            if (succeeded) {
+                [CommonUtil alert:@"regiester succeed"];
+                [self performSegueWithIdentifier:@"registerDoneSegue" sender:nil];
+            }else{
+                if(error){
+                    [CommonUtil alert:error.localizedDescription];
+                }else{
+                    [CommonUtil alert:@"error happen when registering"];
+                }
+            }
+        }];
+    }
+}
+
+-(BOOL)isInputValid{
+    BOOL returnValue=YES;
+    
+    if([self isBlankString:self.emailTextField.text]||[self isBlankString:self.passwordTextField.text]||[self isBlankString:self.repeatTextField.text]||[self isBlankString:self.userNameTextField.text]){
+        [CommonUtil alert:@"exist blank input"];
+        returnValue=NO;
+    }
+    
+    NSString *password=self.passwordTextField.text;
+    NSString *repeatPassword=self.repeatTextField.text;
+    
+    if(![password isEqualToString:repeatPassword]){
+        [CommonUtil alert:@"password not same"];
+        returnValue=NO;
+    }
+    
+    return returnValue;
+}
+
+- (BOOL) isBlankString:(NSString *)string {
+    if (string == nil || string == NULL) {
+        return YES;
+    }
+    if ([string isKindOfClass:[NSNull class]]) {
+        return YES;
+    }
+    if ([[string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] length]==0) {
+        return YES;
+    }
+    return NO;
+}
 
 @end
